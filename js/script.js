90667d014d3f845894cdd2416a7c40b1f7973e9f
akeek <svein.ek@stud.noroff.no>
2022-02-24 19:59:08 +0100

    start of project

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+const htmlContainer = document.querySelector(".text")
+
+
+async function makeApiCall() {
+    try {
+        const response = await fetch(url);
+
+        const json = await response.json();
+
+        const results = json
+
+        console.log(results);
+
+        htmlContainer.innerHTML = ""
+
+        for(let i = 0; i < results.length; i++){
+
+            if(i === 10) {
+                break;
+            }
+
+
+            htmlContainer.innerHTML += `<div class="resultsContainer">
+                                        <h2><a href="details.html">${results[i].name}</a></h2> 
+                                        <h3>Product type: ${results[i].product_type}</h3> 
+                                        <h3>Price: $${results[i].price}</h3> 
+                                        <div class="details-image" 
+                                    style="background-image: url('${results[i].image_link}')"></div>
+                                    </div>`
+        }
+
+        
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+makeApiCall();
+
